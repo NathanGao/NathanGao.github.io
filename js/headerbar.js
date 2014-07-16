00bb090b8f808eff845cdd9bd00c88a6e2bce07a
NFG <nathan.gao@gmail.com>
2014-07-16 16:55:48 -0400

    flex header tweak

--- js/headerbar.js
+++ js/headerbar.js
@@ -25,18 +25,18 @@ $(window).scroll(function(){
     var $nav = $('.headertitle');
     if ($('body').scrollTop() > 70) {
         if ($nav.data('size') == 'big') {
-            $nav.data('size','small').animate({fontSize : '2.0vmin'}, 60);
+            $nav.data('size','small').animate({fontSize : '4.0vmin'}, 60);
         }
     } else {
         if ($nav.data('size') == 'small') {
-            $nav.data('size','big').animate({fontSize : '2.5vmin'}, 100);
+            $nav.data('size','big').animate({fontSize : '8.5vmin'}, 100);
         }
     }
 
     var $nav = $('.headertext');
     if ($('body').scrollTop() > 70) {
         if ($nav.data('size') == 'big') {
-            $nav.data('size','small').animate({fontSize : '1vmin'}, 20);
+            $nav.data('size','small').animate({fontSize : '1.5vmin'}, 20);
             $nav.data('size','small').animate({paddingLeft: '30%'}, 50);
             $nav.data('size','small').animate({top: '32%'}, 50);
         }
@@ -46,7 +46,7 @@ $(window).scroll(function(){
             // $nav.data('size','big').animate({fontSize : '14px'}, 40);
             $nav.data('size','big').animate({top: '55%'}, 50);
             $nav.data('size','big').animate({paddingLeft: '0%'}, 50);
-            $nav.data('size','big').animate({fontSize : '1.3vmin'}, 20);
+            $nav.data('size','big').animate({fontSize : '2.0vmin'}, 20);
 
         }
     }
