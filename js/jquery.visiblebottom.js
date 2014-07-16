eb451104a8eab2d7527dc5c160c8426df00ba47c
NFG <nathan.gao@gmail.com>
2014-07-16 15:51:36 -0400

    test scroll change

--- js/jquery.visiblebottom.js
+++ js/jquery.visiblebottom.js
@@ -22,7 +22,7 @@
           compareTop    = partial === true ? _bottom : _top,
           compareBottom = partial === true ? _top : _bottom;
     
-    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+    return ((compareBottom <= viewBottom));
 
   };
     
