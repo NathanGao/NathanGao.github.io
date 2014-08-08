db0de97fea801c11db55cb6a92b023f20a306799
NFG <nathan.gao@gmail.com>
2014-08-08 02:15:24 -0400

    font test, blur working

--- js/sectionhighlight.js
+++ js/sectionhighlight.js
@@ -1,18 +1,52 @@
 $(function(){
-    $('html').data('state','inactive');
+    $('#intro').data('state','active');
+    $('#projectmalbec').data('state','inactive');
+    $('#projectperseus').data('state','inactive');
+    $('#projectpoop').data('state','inactive');
+
+
 });
 
 $(window).scroll(function(){
 
 
-    var $nav = $('html');
-    if ($('body').scrollTop() > 500) {
+    var $nav = $('#intro');
+    if (($('body').scrollTop() >= 0 ) && ($('body').scrollTop() < 500 )) {
         if ($nav.data('state') == 'inactive') {
-            $nav.data('state','active').animate({backgroundColor : '#e67e22'}, 100);
+/*            $nav.data('state','active').animate({backgroundColor : 'rgba(243, 156, 18,.5)'}, 400);*/
+            $nav.data('state','active').removeClass("gradient");
+
         }
     } else {
         if ($nav.data('state') == 'active') {
-            $nav.data('state','inactive').animate({backgroundColor: '#fff'}, 100);
+/*            $nav.data('state','inactive').animate({backgroundColor: 'transparent'}, 400);*/
+            $nav.data('state','inactive').addClass("gradient");
+        }
+    }
+
+
+    var $nav = $('#projectmalbec');
+    if (($('body').scrollTop() > 500 ) && ($('body').scrollTop() < 2400 )) {
+        if ($nav.data('state') == 'inactive') {
+/*            $nav.data('state','active').animate({backgroundColor : 'rgba(243, 156, 18,.5)'}, 400);*/
+            $nav.data('state','active').removeClass("gradient");
+
+        }
+    } else {
+        if ($nav.data('state') == 'active') {
+/*            $nav.data('state','inactive').animate({backgroundColor: 'transparent'}, 400);*/
+            $nav.data('state','inactive').addClass("gradient");
+        }
+    }
+
+    var $nav = $('#projectperseus');
+    if (($('body').scrollTop() > 2400 ) && ($('body').scrollTop() < 3500 )) {
+        if ($nav.data('state') == 'inactive') {
+            $nav.data('state','active').removeClass("gradient");
+        }
+    } else {
+        if ($nav.data('state') == 'active') {
+            $nav.data('state','inactive').addClass("gradient");
         }
     }
 
@@ -21,9 +55,18 @@ $(window).scroll(function(){
 
 
 
-
 );
 
- /*            $nav.data('state','big').append("<p>Test</p>");*/
+     var win = $(window);
 
- /*.animate({box-shadow: '12px 12px #000000'}, 100*/
+
+win.scroll(function(event) {
+  
+  allMods.each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("come-in"); 
+    } 
+  });
+  
+});
