2e86bf722fde0072520304a7165cd2d428e96575
NFG <nathan.gao@gmail.com>
2014-10-02 00:27:07 -0400

    cleaned text

--- js/headerbar.js
+++ js/headerbar.js
@@ -28,12 +28,14 @@ $(window).scroll(function(){
     if ($('body').scrollTop() >= 150) {
         if ($nav.data('size') == 'big') {
             $nav.data('size','small').stop();
-            $nav.data('size','small').hide({easing:'easeOutBack'}, 500);
+            // $nav.data('size','small').hide({easing:'easeOutBack'}, 500);
+            $nav.data('size','small').animate({marginTop: '-30%', easing:'linear'}, 500);
         }
     } else {
         if ($nav.data('size') == 'small') {
             $nav.data('size','big').stop();
-            $nav.data('size','big').show({easing:'easeOutBack'}, 500);
+            // $nav.data('size','big').show({easing:'easeOutBack'}, 500);
+            $nav.data('size','big').animate({marginTop: '-13%', easing:'linear'}, 500);
 
         }
     }
