56787482afacefd798350e6df512d1928ee0c311
NFG <nathan.gao@gmail.com>
2017-06-11 01:27:47 -0400

    reinit

---
+++ Portfolio2017/files/page_3/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j],k,_(l,m,n,o,p,q,r,_(),s,_(t,u,v,w,x,_(y,z,A,B),C,null,D,E,F,E,G,H,I,null,J,K,L,M,N,O,P,K),Q,_(R,_(),S,_(),T,_()),U,_(),V,_(W,[])),X,_(),Y,_());}; 
+var b="url",c="page_3.html",d="generationDate",e=new Date(1497156984705.5),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="Baiklo",k="page",l="packageId",m="3c13d515efd74c1aa550058fe7f2de06",n="type",o="Axure:Page",p="name",q="Page 3",r="notes",s="style",t="baseStyle",u="627587b6038d43cca051c114ac41ad32",v="pageAlignment",w="center",x="fill",y="fillType",z="solid",A="color",B=0xFFFFFFFF,C="image",D="imageHorizontalAlignment",E="near",F="imageVerticalAlignment",G="imageRepeat",H="auto",I="favicon",J="sketchFactor",K="0",L="colorStyle",M="appliedColor",N="fontName",O="Applied Font",P="borderWidth",Q="adaptiveStyles",R="a",S="b",T="c",U="interactionMap",V="diagram",W="objects",X="masters",Y="objectPaths";
+return _creator();
+})());
